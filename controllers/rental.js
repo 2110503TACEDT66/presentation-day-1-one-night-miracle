a7df018d49d38ce4a69e4f86f50adaab074ad2c9
Goddaniel96 <glodxisreal96@gmail.com>
2024-03-04 17:41:49 +0700

    change status201

--- controllers/rental.js
+++ controllers/rental.js
@@ -87,7 +87,7 @@ exports.createRental=async (req,res,next)=>{
     }
     
     const rental= await Rental.create(req.body);
-         res.status(200).json({
+         res.status(201).json({
             success:true,
             data:rental
          });
